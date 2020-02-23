3320a133195db3143d860a59e5149407362b7147
Oskar Pedersen <oskar.pedersen1@gmail.com>
2020-02-23 21:03:24 +0100

    Started on a new shader

--- ojgl/examples/shaders/cachedGeometry.fs
+++ ojgl/examples/shaders/cachedGeometry.fs
@@ -10,6 +10,18 @@ uniform vec2 iResolution;
 
 #define PI 3.1415
 
+#define MAT_FLOOR 1.0
+#define MAT_AUDIENCE 2.0
+#define MAT_SCENE 3.0
+
+vec2 un(vec2 res1, vec2 res2) {
+	if (res1.x < res2.x) {
+		return res1;
+	} else {
+		return res2;
+	}
+}
+
 void moda (inout vec2 p, float rep)
 {
     float per = 2.*PI/rep;
@@ -59,31 +71,52 @@ float fractalBox(vec3 p) {
    return d;
 }
 
+vec2 audience(vec3 p) {
+	float s = 5.0;
+	float qx = mod(p.x + s * 0.5, s) - s * 0.5;
+	float qz = mod(p.z + s * 0.5, s) - s * 0.5;
+	vec3 q = vec3(qx, p.y, qz);
+	return vec2(sdBox(q, vec3(0.5, 2.0, 0.5)), MAT_AUDIENCE);
+}
+
+vec2 scene(vec3 p) {
+	vec3 t = p - vec3(0, 0, 50);
+	float dBase = sdBox(t, vec3(20, 4, 5));
+	float d = dBase;
+
+	float dTower = sdBox(t - vec3(0, 0, 2), vec3(5, 50, 2));
+	d = min(d, dTower);
+
+	return vec2(d, MAT_SCENE);
+}
+
+vec2 map(vec3 p) {
+	vec2 res = vec2(p.y, MAT_FLOOR);
+	res = un(res, audience(p));
+	res = un(res, scene(p));
+	return res;
+}
+
 void main(){
 	const vec2 uv = fragCoord.xy;
-    const vec3 ro = vec3(0.0, 0.0, 2.5);
-    const vec3 rd = normalize(vec3(1.0, uv.y - 0.5, uv.x - 0.5));
+    const vec3 ro = vec3(0.0, 10.0, 2.5);
+    const vec3 rd = normalize(vec3(uv.x - 0.5, uv.y - 0.5 - 0.25, 1.0));
+	// TODO: proper target etc
     float t = 0.0;
     vec3 color = vec3(0.0);
     
-    for (int i = 0; i < 10000000; i++) {
+    for (int i = 0; i < 1000; i++) {
     	vec3 p = ro + rd * t;
-        vec3 q = mod(p, 5.0) - 2.5;
-        vec3 r = p / 8.0;
-        float d = fractalBox(q);
+        float d = map(p).x;
         
         if (d < 0.01) {
-            color = vec3(0.5 + 0.5 * sin(r * 1.3));
-            
-            const vec3 lpos = ro + vec3(-1.0, 0, 0);
-            const float dis = length(lpos - p);
-            const vec3 invLight = normalize(lpos - p);
+            color = vec3(1.0, 0.0, 0.0);
             
             vec3 normal;
             const vec3 ep = vec3(0.01, 0, 0);
-            normal.x = fractalBox(q + ep.xyz) - fractalBox(q - ep.xyz);
-            normal.y = fractalBox(q + ep.yxz) - fractalBox(q - ep.yxz);
-            normal.z = fractalBox(q + ep.yzx) - fractalBox(q - ep.yzx);
+            normal.x = map(p + ep.xyz).x - map(p - ep.xyz).x;
+            normal.y = map(p + ep.yxz).x - map(p - ep.yxz).x;
+            normal.z = map(p + ep.yzx).x - map(p - ep.yzx).x;
             normal = normalize(normal);
             
 			fragNormal.rgb = normal;
