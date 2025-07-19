9362fc1e6b2ccb39aaa0ef050fc5ad44ddeb4b50
Oskar Pedersen <oskar.pedersen1@gmail.com>
2025-07-19 12:37:44 +0200

    Ufo top part

--- ojgl/examples/shaders/edison2025/ufo_scenes.fs
+++ ojgl/examples/shaders/edison2025/ufo_scenes.fs
@@ -397,13 +397,17 @@ float boatSplit(vec3 p, float dir)
     return h;
 }
 
+
 float ufo(in vec3 p)
 {
     p -= ufoPos();
     float d2 = sdTorus(p - vec3(0, -3, 0), vec2(8.5, 0.5));
 
-    float d1 = length(p) - (2.0 + max(0.5 - C_1_S*3, 0));
-    return min(d1, d2);
+    float d1 = length(p) - (1.5 + max(0.5 - C_1_S*3, 0));
+
+    float d3 = sdTorus(p - vec3(0, -0.5, 0), vec2(1.5, 0.5));
+
+    return min(d2, smink(d1, d3, 1.5));
 }
 
 DistanceInfo sunk(DistanceInfo a, DistanceInfo b, float k) {
